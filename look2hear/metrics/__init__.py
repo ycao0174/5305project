ef8a45d724fecabb49511ad367913bdec52b8f35
ycao0174 <141391380+ycao0174@users.noreply.github.com>
2023-11-10 22:42:23 +1100

    Update __init__.py

--- look2hear/metrics/__init__.py
+++ look2hear/metrics/__init__.py
@@ -1,9 +1,3 @@
-###
-# Author: Kai Li
-# Date: 2021-06-22 12:22:41
-# LastEditors: Kai Li
-# LastEditTime: 2021-07-14 19:15:22
-###
 from .wrapper import MetricsTracker
 from .splitwrapper import SPlitMetricsTracker
 
